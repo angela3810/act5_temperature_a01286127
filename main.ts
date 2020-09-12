2b99408a762ddac22217f7f5ffd17072964f3088
angela3810 <70651967+angela3810@users.noreply.github.com>
2020-09-11 19:51:27 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,25 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(Celcius)
+    Celcius = input.temperature()
     basic.showIcon(IconNames.Happy)
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(Kelvin)
+    Kelvin = input.temperature() + 273.15
+    basic.showIcon(IconNames.Confused)
+})
 input.onButtonPressed(Button.B, function () {
-    let Fahrenheit = 0
     basic.showNumber(Fahrenheit)
+    Fahrenheit = input.temperature() * (9 / 5) + 32
     basic.showIcon(IconNames.Sad)
 })
+let Kelvin = 0
+let Fahrenheit = 0
 let Celcius = 0
 basic.showString("Angela")
 basic.showString("A01286127")
 Celcius = input.temperature()
-basic.forever(function () {
-	
-})
+Fahrenheit = input.temperature()
+Kelvin = input.temperature()
+basic.pause(1000)
+basic.clearScreen()
